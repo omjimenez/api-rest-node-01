14d51c2aaec8712a8413cd9bce4cdd71f8655609
Orlando Jimenez <orlando.jmz13@gmail.com>
2021-08-16 23:18:57 -0500

    se mojora la implementacion de rutas

--- routes/places.js
+++ routes/places.js
@@ -5,69 +5,12 @@ const Place = require('../models/Place');
 let router = express.Router();
 
 router.route('/')
-.get((req, res) => {
-    Place.find({})
-      .then(docs => {
-        res.json(docs);
-      })
-      .catch(err => {
-        console.log(err);
-        res.json(err);
-      })
-  })
-  .post((req, res) =>  {
-    Place.create({
-      title: req.body.title,
-      description: req.body.description,
-      acceptsCreditCard: req.body.acceptsCreditCard,
-      openHour: req.body.openHour,
-      closeHour: req.body.closeHour
-    }).then( doc =>{
-      res.json(doc)
-    }).catch(err => {
-      console.log(err)
-    });
-  })
+.get()
+.post()
 
 router.route('/:id')
-.get((req, res) =>{
-    Place.findById(req.params.id)
-        .then(doc =>{
-        res.json(doc);
-        })
-        .catch(err => {
-        console.log(err);
-        res.json(err);
-        })
-    })
-.put((req, res) => {
-    let placeParams = {};
-    let attributes = ['title', 'description', 'acceptsCreditCard', 'openHour', 'closeHour'];
-  
-    attributes.forEach(attr => {
-      if (Object.prototype.hasOwnProperty.call(req.body, attr))
-        placeParams[attr] = req.body[attr]
-  
-    });
-  
-    Place.findOneAndUpdate({'_id': req.params.id}, placeParams, {new: true})
-    .then(doc =>{
-      res.json(doc);
-    })
-    .catch(err => {
-      console.log(err);
-      res.json(err);
-    })
-  })
-  .delete((req, res) =>{
-    Place.findByIdAndDelete(req.params.id)
-    .then(doc => {
-      res.json({});
-    })
-    .catch(err => {
-      console.log(err);
-      res.json(err);
-    })
-  })
+.get()
+.put()
+.delete()
   
 module.exports = router;
